8863269ea597918e608261efefc62e615cbea88a
Rakshith Acharya <rakshixh@gmail.com>
2024-11-24 00:45:10 +0530

    🎮 Added config.js (default values)

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  defaultSizeLimit: 50000, // Default size limit for large files in bytes
+  defaultDirectory: "./reports", // Default directory to save reports
+  defaultOutput: "html", // Default report format (html/json)
+};
