4bd8316e021ea20cbe259fae14ab06f21c7a9e64
Rakshith Acharya <rakshixh@gmail.com>
2024-11-26 23:45:11 +0530

    🏵 Added default for maxlines

--- config.js
+++ config.js
@@ -2,4 +2,5 @@ module.exports = {
   defaultSizeLimit: 50000, // Default size limit for large files in bytes
   defaultDirectory: "./reports", // Default directory to save reports
   defaultOutput: "html", // Default report format (html/json)
+  defaultMaxLines: 500, // Default maximum lines of code allowed in a single file
 };
